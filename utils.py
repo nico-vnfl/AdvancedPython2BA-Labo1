56b977d16237b666b71a76a6aebc806904ed33f2
Nicolas Vannuffel <24350@ecam.be>
2026-04-02 20:56:59 +0200

    Implement all functions

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import math
+from scipy.integrate import quad
+
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -5,7 +8,14 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	if n < 0:
+		raise ValueError("n must be >= 0")
+	
+	result = 1
+	for i in range(1, n+1): #car range s'arrête au dernier elem non compris
+		result *= i
+	return result
+	
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -14,14 +24,30 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	if a == 0:
+		if b == 0:
+			return ()
+		return (-c/b,)
+	
+	delta = b*b - 4*a*c
+
+	if delta < 0:
+		return ()
+	elif delta ==0:
+		return (-b/(2*a),)
+	else:
+		sqrt_delta = math.sqrt(delta)
+		x1 = (-b-sqrt_delta) / (2*a)
+		x2 = (-b+sqrt_delta) / (2*a)
+		return (x1, x2)
+	
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
 	
 	Pre: 'function' is a valid Python expression with x as a variable,
 		'lower' <= 'upper',
-		'function' continuous and integrable between 'lower‘ and 'upper'.
+		'function' continuous and integrable between 'lower' and 'upper'.
 	Post: Returns an approximation of the integral from 'lower' to 'upper'
 		of the specified 'function'.
 
@@ -29,7 +55,13 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	#créer une fonction python apd string
+	def f(x):
+		return eval(function, {"x": x})
+	#quad retourne (resultat, erreur)
+	result, _ = quad(f, lower, upper)
+	return result
+
 
 if __name__ == '__main__':
 	print(fact(5))
